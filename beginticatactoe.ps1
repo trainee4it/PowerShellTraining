033a286a641e64d2fb1f06bf95af90e1c94c649a
trainee4it <69849181+trainee4it@users.noreply.github.com>
2021-06-28 05:48:45 -0700

    Add files via upload

---
+++ beginticatactoe.ps1
@@ -0,0 +1,32 @@
+﻿$BoardArray = @('1','2','3','4','5','6','7','8','9')
+
+
+
+Write-Host "-------------"
+Write-Host "|"$BoardArray[0] "|" $BoardArray[1]"|" $BoardArray[2] "|"
+Write-Host "-------------"
+Write-Host "|"$BoardArray[3] "|" $BoardArray[4]"|" $BoardArray[5] "|"
+Write-Host "-------------"
+Write-Host "|"$BoardArray[6] "|" $BoardArray[7]"|" $BoardArray[8] "|"
+Write-Host "-------------"
+
+$BoardArray[0] = 'X'
+
+Write-Host "-------------"
+Write-Host "|"$BoardArray[0] "|" $BoardArray[1]"|" $BoardArray[2] "|"
+Write-Host "-------------"
+Write-Host "|"$BoardArray[3] "|" $BoardArray[4]"|" $BoardArray[5] "|"
+Write-Host "-------------"
+Write-Host "|"$BoardArray[6] "|" $BoardArray[7]"|" $BoardArray[8] "|"
+Write-Host "-------------"
+
+
+$BoardArray[1] = 'O'
+
+Write-Host "-------------"
+Write-Host "|"$BoardArray[0] "|" $BoardArray[1]"|" $BoardArray[2] "|"
+Write-Host "-------------"
+Write-Host "|"$BoardArray[3] "|" $BoardArray[4]"|" $BoardArray[5] "|"
+Write-Host "-------------"
+Write-Host "|"$BoardArray[6] "|" $BoardArray[7]"|" $BoardArray[8] "|"
+Write-Host "-------------"
