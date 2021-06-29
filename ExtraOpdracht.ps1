2f10961cc61079bf0bc0793d03b857c59f646e09
trainee4it <69849181+trainee4it@users.noreply.github.com>
2021-06-29 11:50:37 +0200

    Add files via upload

---
+++ ExtraOpdracht.ps1
@@ -0,0 +1,12 @@
+﻿$usersToImport = Import-Csv -Path C:\app\users.csv
+
+
+foreach($item in $usersToImport)
+{
+
+    $SecurePassword = ConvertTo-SecureString -String 'P@ssw0rd' -AsPlainText -Force
+
+    New-LocalUser -Name $item.naam -Description $item.afdeling -Password $SecurePassword
+
+
+}
